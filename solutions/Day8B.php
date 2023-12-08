a255893eab876aa45b67c91d3c7a5279c6446910
Sarina Corrigan <sarina.corrigan@gmail.com>
2023-12-08 01:04:08 -0500

    Clean up solution 8b

--- solutions/Day8B.php
+++ solutions/Day8B.php
@@ -1,11 +1,7 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', true);
-ini_set('display_startup_errors', true);
+
 class Day8B extends BaseSolution
 {
-    private array $cardStrengthMap = [];
-
     protected function solution(string $input): string
     {
         $lines = explode("\n", $input);
